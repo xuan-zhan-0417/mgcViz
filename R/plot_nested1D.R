0804ff2f4965d2bc467db9986aee7a718ca3ab04
xuan-zhan-0417 <er25922@bristol.ac.uk>
2026-09-24 15:15:54 +0100

    reformat plot.nested1D to match plot.nested2D's code style (no logic changes)

--- R/plot_nested1D.R
+++ R/plot_nested1D.R
@@ -29,68 +29,43 @@
 #' @export plot.nested1D
 #' @export
 #'
-plot.nested1D <- function(x,
-                          inner = FALSE,
-                          smooth = FALSE,
-                          n = 100,
-                          xlim = NULL,
-                          ylim = NULL,
-                          maxpo = 1e4,
-                          trans = identity,
-                          ci = TRUE,
-                          a_in = FALSE,
-                          ...)  {
+plot.nested1D <- function(x, inner = FALSE, smooth = FALSE, n = 100, xlim = NULL, ylim = NULL,
+                          maxpo = 1e4, trans = identity, ci = TRUE, a_in = FALSE, ...) {
+
   if (inner) {
     # 1) Prepare data
-    P <- .prepareInnerNested(
-      o = x,
-      n = n,
-      xlim = xlim,
-      ylim = ylim,
-      smooth = smooth,
-      a_in,
-      ...
-    )
-    
-    out <- .plot.inner.nested.smooth.1D(P = P,
-                                        trans = trans,
-                                        maxpo = maxpo,
-                                        ci = ci)
-    
+    P <- .prepareInnerNested(o = x, n = n, xlim = xlim, ylim = ylim, smooth = smooth,
+                             a_in, ...)
+
+    out <- .plot.inner.nested.smooth.1D(P = P, trans = trans, maxpo = maxpo, ci = ci)
+
   } else {
     # 1) Prepare data
-    P <- .prepareOuterNested(o = x,
-                             n = n,
-                             xlim = xlim,
-                             ...)
-    
+    P <- .prepareOuterNested(o = x, n = n, xlim = xlim, ...)
+
     # 2) Produce output object
-    out <- .plot.outer.nested.1D(
-      x = P$smooth,
-      P = P,
-      trans = trans,
-      maxpo = maxpo
-    )
+    out <- .plot.outer.nested.1D(x = P$smooth, P = P, trans = trans, maxpo = maxpo)
   }
-  
+
   class(out) <- c("plotSmooth", "gg")
-  
+
   return(out)
+
 }
 
 ########################
 #' @noRd
 .plot.outer.nested.1D <- function(x, P, trans, maxpo) {
+
   .dat <- list()
-  
+
   if (!is.null(P$raw)) {
     # Construct data.frame of partial residuals
     res <- data.frame("x" = as.vector(P$raw))
-    
+
     # Exclude residuals falling outside boundaries
-    .dat$res <- res[res$x >= P$xlim[1] &
-                      res$x <= P$xlim[2], , drop = FALSE]
-    
+    .dat$res <- res[res$x >= P$xlim[1] & res$x <= P$xlim[2], , drop = FALSE]
+
     # Sample if too many points (> maxpo)
     nres <- nrow(.dat$res)
     .dat$res$sub <- if (nres > maxpo) {
@@ -99,128 +74,83 @@ plot.nested1D <- function(x,
       rep(T, nres)
     }
   }
-  
-  .dat$fit <- data.frame(
-    x = P$x,
-    y = P$fit,
-    ty = trans(P$fit),
-    se = P$se
-  )
+
+  .dat$fit <- data.frame(x = P$x, y = P$fit, ty = trans(P$fit), se = P$se)
   .dat$misc <- list(trans = trans)
-  
+
   .pl <- ggplot(data = .dat$fit, mapping = aes(x = x, y = y)) +
-    labs(title = P$main,
-         x = P$xlab,
-         y = P$ylab) + theme_bw() +
-    theme(panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank())
-  
-  return(list(
-    "ggObj" = .pl,
-    "data" = .dat,
-    type = "1D"
-  ))
+    labs(title = P$main, x = P$xlab, y = P$ylab) +
+    theme_bw() +
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+
+  return(list("ggObj" = .pl, "data" = .dat, type = "1D"))
+
 }
 
 ########################
 #' @noRd
 .plot.inner.nested.smooth.1D <- function(P, trans, maxpo, ci) {
+
   if (is.null(P)) {
     return(NULL)
   }
-  
+
   if (P$type == "si_nexpsm_coef") {
     # plot coef [alpha_si]
     .dat <- list()
-    .dat$fit <- data.frame(
-      "x"        = P$x,
-      "y"        = P$fit,
-      "ty"       = trans(P$fit),
-      "se"       = P$se,
-      "lower"    = P$lower,
-      "upper"    = P$upper,
-      "ty_lower" = trans(P$lower),
-      "ty_upper" = trans(P$upper)
-    )
+    .dat$fit <- data.frame("x" = P$x, "y" = P$fit, "ty" = trans(P$fit), "se" = P$se,
+                           "lower" = P$lower, "upper" = P$upper,
+                           "ty_lower" = trans(P$lower), "ty_upper" = trans(P$upper))
     .dat$misc <- list("trans" = trans)
-    
+
     .pl <- ggplot(data = .dat$fit, aes(x = x, y = ty))
-    .pl <- .pl + 
-      geom_errorbar(aes(ymin = ty_lower, ymax = ty_upper), 
-                    linetype = "dashed")
-    
+    .pl <- .pl +
+      geom_errorbar(aes(ymin = ty_lower, ymax = ty_upper), linetype = "dashed")
+
     .pl <- .pl +
       geom_point(color = "black", size = 2) +
-      labs(title = P$main,
-           x = P$xlab,
-           y = P$ylab) +
+      labs(title = P$main, x = P$xlab, y = P$ylab) +
       theme_bw() +
-      theme(panel.grid.major = element_blank(),
-            panel.grid.minor = element_blank())
-    
+      theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+
     # if ci is FALSE, we still want to show the error bars (dashed lines), but not the points.
     if (!ci) {
-      .pl <- .pl + coord_cartesian(ylim = c(0, min(1, (max(.dat$fit$ty, na.rm = TRUE)+0.1))))
+      .pl <- .pl + coord_cartesian(ylim = c(0, min(1, (max(.dat$fit$ty, na.rm = TRUE) + 0.1))))
     }
-    
-    out <- list(
-      "ggObj" = .pl,
-      "data"  = .dat,
-      "type"  = c("si", "Factor")
-    )
-    
-  }else if (P$type == "si_nexpsm_xa") {
+
+    out <- list("ggObj" = .pl, "data"  = .dat, "type"  = c("si", "Factor"))
+
+  } else if (P$type == "si_nexpsm_xa") {
     # plot data after smooth
     .dat <- list()
-    .dat$fit <- data.frame(
-      "x"  = P$x,
-      "y"  = P$fit,
-      "ty" = trans(P$fit),
-      "se" = NA
-    )
+    .dat$fit <- data.frame("x" = P$x, "y" = P$fit, "ty" = trans(P$fit), "se" = NA)
     .dat$misc <- list("trans" = trans)
-    
+
     .pl <- ggplot(data = .dat$fit, aes(x = x, y = y)) +
-      labs(title = P$main,
-           x = P$xlab,
-           y = P$ylab) +
-      theme_bw() + theme(panel.grid.major = element_blank(),
-                         panel.grid.minor = element_blank())
-    
-    out <- list("ggObj" = .pl,
-                "data" = .dat,
-                "type" = "1D")
-    
+      labs(title = P$main, x = P$xlab, y = P$ylab) +
+      theme_bw() +
+      theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+
+    out <- list("ggObj" = .pl, "data" = .dat, "type" = "1D")
+
   }
-  
+
   if (P$type == "nexpsm") {
-    out <- .plot.mgcv.smooth.1D(
-      x = NULL,
-      P = P,
-      trans = trans,
-      maxpo = maxpo
-    )
-    
+    out <- .plot.mgcv.smooth.1D(x = NULL, P = P, trans = trans, maxpo = maxpo)
+
   }
-  
+
   if (P$type == "mgks") {
     .dat <- list()
-    .dat$fit <- data.frame(
-      "z"  = drop(P$fit),
-      "tz" = drop(trans(P$fit)),
-      "x"  = rep(P$x, length(P$fit) / length(P$x)),
-      "y"  = rep(P$y, each = length(P$fit) / length(P$x)),
-      "se" = P$se
-    )
-    
-    P$raw <- data.frame(z = P$p.resid,
-                        x = P$X0[, 1],
-                        y = P$X0[, 2])
+    .dat$fit <- data.frame("z" = drop(P$fit), "tz" = drop(trans(P$fit)),
+                           "x" = rep(P$x, length(P$fit) / length(P$x)),
+                           "y" = rep(P$y, each = length(P$fit) / length(P$x)), "se" = P$se)
+
+    P$raw <- data.frame(z = P$p.resid, x = P$X0[, 1], y = P$X0[, 2])
     if (!is.null(P$raw)) {
       .dat$res <- P$raw[P$raw$x >= P$xlim[1] & P$raw$x <= P$xlim[2] &
-                          P$raw$y >= P$ylim[1] &
-                          P$raw$y <= P$ylim[2] , , drop = FALSE]
-      
+                          P$raw$y >= P$ylim[1] & P$raw$y <= P$ylim[2], , drop = FALSE]
+
       nres <- nrow(.dat$res)
       .dat$res$sub <- if (nres > maxpo) {
         sample(c(rep(TRUE, maxpo), rep(FALSE, nres - maxpo)))
@@ -228,51 +158,34 @@ plot.nested1D <- function(x,
         rep(TRUE, nres)
       }
     }
-    
+
     .dat$misc <- list("trans" = trans)
-    
+
     .pl <- ggplot(data = .dat$fit, aes(x = x, y = y, z = z)) +
-      labs(title = P$main,
-           x = P$xlab,
-           y = P$ylab) +
+      labs(title = P$main, x = P$xlab, y = P$ylab) +
       theme_bw() +
-      theme(panel.grid.major = element_blank(),
-            panel.grid.minor = element_blank())
-    
-    out <- list(
-      "ggObj" = .pl,
-      "data" = .dat,
-      "type" = c("mgks", "2D")
-    )
-    
+      theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+
+    out <- list("ggObj" = .pl, "data" = .dat, "type" = c("mgks", "2D"))
+
   }
-  
+
   if (P$type == "si") {
     .dat <- list()
-    .dat$fit <- data.frame(
-      "x"  = as.factor(P$x),
-      "y"  = unname(P$fit),
-      "ty" = trans(unname(P$fit)),
-      "se" = unname(P$se)
-    )
+    .dat$fit <- data.frame("x" = as.factor(P$x), "y" = unname(P$fit),
+                           "ty" = trans(unname(P$fit)), "se" = unname(P$se))
     .dat$misc <- list("trans" = trans)
-    
+
     .pl <- ggplot(data = .dat$fit, aes("x" = x, "y" = ty)) +
-      labs(title = P$main,
-           x = P$xlab,
-           y = P$ylab) +
+      labs(title = P$main, x = P$xlab, y = P$ylab) +
       theme_bw() +
-      theme(panel.grid.major = element_blank(),
-            panel.grid.minor = element_blank())
-    
-    out <- structure(list(
-      "ggObj" = .pl,
-      "data" = .dat,
-      "type" = c("si", "Factor")
-    ),
-    class = c("plotSmooth", "gg"))
-    
+      theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+
+    out <- structure(list("ggObj" = .pl, "data" = .dat, "type" = c("si", "Factor")),
+                     class = c("plotSmooth", "gg"))
+
   }
-  
+
   return(out)
+
 }
